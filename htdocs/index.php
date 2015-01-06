dd7c0efdd23cbc07d00ef00b34f9b12a03bfc6fb
Robert Ewald <r3wald@gmail.com>
2015-01-06 01:01:31 +0100

    logging added

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,6 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 require __DIR__ . '/../config/config.php';
 
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/log/application.log',
+));
+
 $app->get('/', function() {
     return json_encode('test');
 });
